80c7b29f669b6f29eb96e01c9f7121a2588f8964
EkberHasanov <ekber.022004@gmail.com>
2023-06-04 19:49:13 +0400

    Refactor tests for handler_translator method update

--- tests/unit_tests/python_telegram_bot_translator_tests.py
+++ tests/unit_tests/python_telegram_bot_translator_tests.py
@@ -3,10 +3,11 @@ def init_test(adapter_with_mock, mock_translator_service):
 
 
 async def handler_translator_test(adapter, update, context, message):
+    @adapter.handler_translator(message)
     async def test_func(update, context, message):
         assert message == "Hello World"
-    wrapped_func = adapter.handler_translator(test_func, message)
-    result = await wrapped_func(update, context, message)
+
+    result = await test_func(update, context, message)
     assert result is None
 
 
@@ -15,9 +16,9 @@ async def handler_translator_translation_test(adapter, update, context):
     t_s_mock = adapter._translator_service
     w = await t_s_mock.translate_str(message, 'fr')
 
+    @adapter.handler_translator(message)
     async def func_test(update, context, message):
         assert w == "Bonjour le monde"
 
-    wrapped_func = adapter.handler_translator(func_test, message)
-    result = await wrapped_func(update, context, message)
+    result = await func_test(update, context, message)
     assert result is None
